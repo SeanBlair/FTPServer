d3af68073942c57b3b2ed04badacdf73c11cb3b7
Sean Blair <seandibango@gmail.com>
2016-11-25 23:47:00 -0800

    Initial implementation of accept connection, send response, wait for response, parse it and print to console. Needs lots of refactoring, most of code from example, plus my hck...

--- CSftp.c
+++ CSftp.c
@@ -25,7 +25,23 @@
 
 
 
-#define BACKLOG 10
+#define BACKLOG 10      // how many pending connections queue will hold
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
 
 // Here is an example of how to use the above function. It also shows
 // one how to get the arguments passed on the command line.
@@ -35,13 +51,24 @@ int main(int argc, char **argv) {
     
     struct addrinfo hints, *servinfo, *p;  // not sure what these for... look in tutorial.
     int yes=1;
-    int sockfd;
+    int sockfd, new_fd;
+
+    socklen_t sin_size;
+
+    
+    char s[INET6_ADDRSTRLEN];
+
+    struct sockaddr_storage their_addr; // connector's address information
 
     // This is some sample code feel free to delete it
     // This is the main program for the thread version of nc
 
-    int rv;   // return value?
-    
+    int rv, numbytes;   // return value?
+
+    char buf[MAXDATASIZE];
+    char *line = NULL;
+    size_t size;
+
     // Check the command line arguments
     if (argc != 2) {
       usage(argv[0]);
@@ -92,6 +119,9 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+
+    // listen call..
+    // maybe where thread should be started.
     if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
         exit(1);
@@ -101,6 +131,50 @@ int main(int argc, char **argv) {
     printf("CSftp: waiting for connections...\n");
 
 
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server2: got connection from %s\n", s);
+
+        if (send(new_fd, "230 User logged in, proceed.\n", 30, 0) == -1) {
+            perror("send");
+        }
+
+
+        // reading client message #1
+        if ((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) {
+            perror("recv");
+            exit(1);
+        }
+
+        buf[numbytes] = '\0';
+
+        // printing client message #1
+        printf("server2: received %s",buf);
+
+        // read from console... 
+        //char *line = NULL;
+        if (getline(&line, &size, stdin) == -1) {
+            printf("No line\n");
+        } else {
+            // send console message to client
+            char str[MAXDATASIZE];
+            strcpy(str, "500 ");
+            strcat(str, line); 
+            if (send(new_fd, str, (strlen(line) + 4), 0) == -1) {
+            perror("send");
+            }
+        }
+
+    }
 
 
 
