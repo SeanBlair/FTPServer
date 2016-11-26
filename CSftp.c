354081be61b78cc0d55084c59040ebc8385a2760
Sean Blair <seandibango@gmail.com>
2016-11-25 23:24:18 -0800

    Implement program create a socket using the port number specified on the command line and and start listening on that port.

--- CSftp.c
+++ CSftp.c
@@ -6,17 +6,26 @@
 #include "usage.h"
 
 
-///////  from demo server  ////////
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <errno.h>
-#include <string.h>
-// #include <netinet/in.h>
-#include <netdb.h>
-// #include <arpa/inet.h>
-// #include <sys/wait.h>
-// #include <signal.h>
 
+
+///////  from demo server  ////////
+#include <string.h>
+#include <netdb.h>  
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <netinet/in.h>
+  
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+
+
+
+#define BACKLOG 10
 
 // Here is an example of how to use the above function. It also shows
 // one how to get the arguments passed on the command line.
@@ -24,8 +33,9 @@
 int main(int argc, char **argv) {
 
     
-    struct addrinfo hints, *servinfo;  // not sure what these for... look in tutorial.
-
+    struct addrinfo hints, *servinfo, *p;  // not sure what these for... look in tutorial.
+    int yes=1;
+    int sockfd;
 
     // This is some sample code feel free to delete it
     // This is the main program for the thread version of nc
@@ -52,13 +62,54 @@ int main(int argc, char **argv) {
     }
 
 
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL)  {
+        fprintf(stderr, "server2: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+
+    printf("CSftp: waiting for connections...\n");
+
+
+
+
 
 
     // This is how to call the function in dir.c to get a listing of a directory.
     // It requires a file descriptor, so in your code you would pass in the file descriptor 
     // returned for the ftp server's data connection
     
-    printf("Printed %d directory entries\n", listFiles(1, "."));
+    //printf("Printed %d directory entries\n", listFiles(1, "."));
     return 0;
 
 }
