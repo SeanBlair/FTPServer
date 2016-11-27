de96d3598d1664f53773eb8fab128230fdb94b6e
Sean Blair <seandibango@gmail.com>
2016-11-26 23:54:01 -0800

    Major refactoring, listening() method correct type for Pthreads

--- CSftp.c
+++ CSftp.c
@@ -21,24 +21,16 @@
 
 
 
-#define BACKLOG 10      // how many pending connections queue will hold
-#define MAXDATASIZE 100 // max number of bytes we can get at once
+#define BACKLOG 10      // how many pending connections queue will hold TODO (look into)
+#define MAXDATASIZE 100 // max number of bytes we can get at once  TODO (look into)
 
 
+void messageState(int fd) {
 
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-
-void listening(int socket_fd) {
+void * listening(void * socket_fd) {      // int socket_fd
+    
     int tcpfd, numbytes;
 
     char buf[MAXDATASIZE];
@@ -48,49 +40,30 @@ void listening(int socket_fd) {
 
     struct sockaddr_storage their_addr; // connector's address information
     
-    char s[INET6_ADDRSTRLEN];
-    
-    if (listen(socket_fd, BACKLOG) == -1) {
+    if (listen( *(int*) socket_fd, BACKLOG) == -1) {
         perror("listen");
         exit(1);
     }
 
-
-    printf("CSftp: waiting for connections...\n");
-    printf("next line..\n");
-
-
+    printf("CSftp: in listen state.\n");
+    
     sin_size = sizeof their_addr;
-    tcpfd = accept(socket_fd, (struct sockaddr *)&their_addr, &sin_size);
+    tcpfd = accept( *(int*) socket_fd, (struct sockaddr *)&their_addr, &sin_size);
     if (tcpfd == -1) {
         perror("accept");
-        //continue;  //might need to put this code in loop...
+        //continue;  //TODO: might need to put this code in loop to use the continue...
     }
 
-    inet_ntop(their_addr.ss_family,
-        get_in_addr((struct sockaddr *)&their_addr),
-        s, sizeof s);
-    printf("server2: got connection from %s\n", s);
+    // TODO: extract message state to own method
+    //messageState(tcpfd);
 
-        // inet_ntop(their_addr.ss_family,
-        //     get_in_addr((struct sockaddr *)&their_addr),
-        //     s, sizeof s);
-        // printf("CSftp: got connection from %s\n", s);
-    printf("About to send 230\n");
-
-    if (send(tcpfd, "230 User logged in, proceed :)\n", 30, 0) == -1) {
-        printf("error when sending 230\n");
+    if (send(tcpfd, "220 Service ready for new user. :)\n", 36, 0) == -1) {
         perror("send");
     }
 
-    printf("sent 230 without error\n");
-
-
-    printf("Outside while loop..\n");
-
     while(1) {  // main accept() loop
 
-        printf("Inside while loop..\n");
+        printf("CSftp: in message state\n");
 
                 // reading client message
         if ((numbytes = recv(tcpfd, buf, MAXDATASIZE-1, 0)) == -1) {
@@ -103,52 +76,36 @@ void listening(int socket_fd) {
 
         buf[numbytes] = '\0';
 
-        // printing client message #1
-        printf("CSftp: received %s",buf);
+        printf("CSftp: received:  %s",buf);
 
-        memset(&buf[0], 0, sizeof(buf));
+        // it is clearing or flushing buffer (i think)
+        // TODO: look into if required or not 
+        //memset(&buf[0], 0, sizeof(buf));
 
-    
-        if (send(tcpfd, "500 I will keep answering... i'm a machine :(", 46, 0) == -1) {
+        strcpy(str, "500 I will keep answering... i'm a machine :(\n");
+
+        if (send(tcpfd, str, 46, 0) == -1) {
             perror("send");
-        }    
+        }
+
+        printf("CSftp: sent    :  %s",str);
     }
 }
 
 
-
-// Here is an example of how to use the above function. It also shows
-// one how to get the arguments passed on the command line.
-
 int main(int argc, char **argv) {
-
     
     struct addrinfo hints, *servinfo, *p;  // not sure what these for... look in tutorial.
-    int yes=1;
-    int sockfd, new_fd;
+    int yes=1;   
+    int sockfd;
 
-    socklen_t sin_size;
-
-
-    char s[INET6_ADDRSTRLEN];
-
-    struct sockaddr_storage their_addr; // connector's address information
-
-    // This is some sample code feel free to delete it
-    // This is the main program for the thread version of nc
-
-    int rv, numbytes;   // return value?
-
-    char buf[MAXDATASIZE];
-    char *line = NULL;
-    size_t size;
+    int rv, numbytes;   // rv == returnValue
 
     // Check the command line arguments
     if (argc != 2) {
       usage(argv[0]);
       return -1;
     }
-
 
     // Create socket with given port number
 
@@ -161,7 +118,6 @@ int main(int argc, char **argv) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
-
 
     // loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
@@ -193,79 +149,10 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-
     // listen call..
     // maybe where thread should be started.
-    // should maybe extract all following code to helper titled listening();
-    // with all required parameters...
+    listening(&sockfd);
 
-    listening(sockfd);
-
-
-
-
-    // if (listen(sockfd, BACKLOG) == -1) {
-    //     perror("listen");
-    //     exit(1);
-    // }
-
-
-    // printf("CSftp: waiting for connections...\n");
-
-
-    // while(1) {  // main accept() loop
-    //     sin_size = sizeof their_addr;
-    //     new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-    //     if (new_fd == -1) {
-    //         perror("accept");
-    //         continue;
-    //     }
-
-    //     inet_ntop(their_addr.ss_family,
-    //         get_in_addr((struct sockaddr *)&their_addr),
-    //         s, sizeof s);
-    //     printf("CSftp: got connection from %s\n", s);
-
-    //     if (send(new_fd, "230 User logged in, proceed.\n", 30, 0) == -1) {
-    //         perror("send");
-    //     }
-
-
-    //     // reading client message #1
-    //     if ((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) {
-    //         perror("recv");
-    //         exit(1);
-    //     }
-
-    //     buf[numbytes] = '\0';
-
-    //     // printing client message #1
-    //     printf("CSftp: received %s",buf);
-
-    //     // read from console... 
-    //     //char *line = NULL;
-    //     if (getline(&line, &size, stdin) == -1) {
-    //         printf("No line\n");
-    //     } else {
-    //         // send console message to client
-    //         char str[MAXDATASIZE];
-    //         strcpy(str, "500 ");
-    //         strcat(str, line); 
-    //         if (send(new_fd, str, (strlen(line) + 4), 0) == -1) {
-    //         perror("send");
-    //         }
-    //     }
-
-    // }
-
-
-
-
-    // This is how to call the function in dir.c to get a listing of a directory.
-    // It requires a file descriptor, so in your code you would pass in the file descriptor 
-    // returned for the ftp server's data connection
-    
-    //printf("Printed %d directory entries\n", listFiles(1, "."));
     return 0;
 
 }
